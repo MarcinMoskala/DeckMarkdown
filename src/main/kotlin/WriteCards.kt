2054a474d9af4c84308d8347e782c299e426a663
Marcin Moskala <marcinmoskala@gmail.com>
2019-12-07 22:37:00 +0300

    WriteTests: Cloze is stored correctly

--- src/main/kotlin/WriteCards.kt
+++ src/main/kotlin/WriteCards.kt
@@ -1,1 +1,8 @@
-fun writeCards(cards: List<Card>): String = ""
+fun writeCards(cards: List<Card>): String = cards.joinToString(separator = "\n\n") {
+    when (it) {
+        is Card.Basic -> TODO()
+        is Card.BasicAndReverse -> TODO()
+        is Card.Cloze -> it.text
+        is Card.Text -> TODO()
+    }
+}
--- src/test/kotlin/WriteTests.kt
+++ src/test/kotlin/WriteTests.kt
@@ -11,4 +11,19 @@ class WriteTests {
         val expected = ""
         assertEquals(text, text)
     }
+
+    @Test
+    fun `Cloze is stored correctly`() {
+        val cards = listOf(
+            Card.Cloze("This is text {{c1::1}}"),
+            Card.Cloze("And this {{c1::text}} number is {{c2::2}}")
+        )
+        val text = writeCards(cards)
+        val expected = """
+This is text {{c1::1}}
+
+And this {{c1::text}} number is {{c2::2}}
+        """.trimIndent()
+        assertEquals(expected, text)
+    }
 }
