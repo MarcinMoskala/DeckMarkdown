826253c68cda01c56d6998696bd86b25b3156713
Marcin Moskala <marcinmoskala@gmail.com>
2019-12-16 15:14:30 +0100

    Introduce assertThrows that is used on unit tests

---
+++ src/test/kotlin/Utils.kt
@@ -0,0 +1,18 @@
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+inline fun <reified T: Throwable> assertThrows(expectedMessage: String? = null, operation: () -> Unit) {
+    val exception = try {
+        operation()
+        null
+    } catch (t: Throwable) {
+        t
+    }
+
+    assertNotNull(exception) { "No error thrown. Expecting ${T::class.qualifiedName}" }
+    assert(exception is T) { "Caught error is $exception when it should be of type ${T::class.qualifiedName}" }
+    if(expectedMessage != null) {
+        assertEquals(expectedMessage, exception.message, "The message is <${exception.message}> when it should be <$expectedMessage>")
+    }
+}
